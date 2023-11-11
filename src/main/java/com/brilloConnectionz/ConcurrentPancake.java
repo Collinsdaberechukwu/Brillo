2f181291a5447b55db5245d6b08652b6e724fdb9
Collins Daberechukwu <collinsdaberechi20@gmail.com>
2023-11-11 20:41:30 +0100

    All commit

---
+++ src/main/java/com/brilloConnectionz/ConcurrentPancake.java
@@ -0,0 +1,56 @@
+package com.brilloConnectionz;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class ConcurrentPancake {
+    private static final int MAX_SHOPKEEPER = 12;
+    private static final int MAX_USERS = 5;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        System.out.println("Starting time: " + getCurrentTime());
+
+        Random random = new Random();
+        CompletableFuture<Integer> shopkeeperFuture = CompletableFuture.supplyAsync(() -> random.nextInt(MAX_SHOPKEEPER + 1));
+        CompletableFuture<List<Integer>> userFutures = CompletableFuture.supplyAsync(()-> new ArrayList<>(3));
+        int NUMBER_OF_USERS = 3;
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            userFutures.get().add(i,CompletableFuture.supplyAsync(() -> random.nextInt(MAX_USERS + 1)).get());
+        }
+        CompletableFuture<Void> allOf = CompletableFuture.allOf(shopkeeperFuture, userFutures);
+        allOf.join();
+
+        int shopkeeperPancakes = shopkeeperFuture.get();
+        int totalUserPancakes = 0;
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            totalUserPancakes += userFutures.get().get(i);
+        }
+
+        int wastedPancakes = Math.max(0, shopkeeperPancakes - totalUserPancakes);
+        boolean metAllNeeds = totalUserPancakes <= shopkeeperPancakes;
+
+        System.out.println("Ending time: " + getCurrentTime());
+        System.out.println("Shopkeeper pancakes: " + shopkeeperPancakes);
+        System.out.print("User pancakes: ");
+        for (int i = 0; i < NUMBER_OF_USERS; i++) {
+            try {
+                System.out.print(userFutures.get().get(i) + " ");
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+        System.out.println("Met all needs: " + metAllNeeds);
+        System.out.println("Wasted pancakes: " + wastedPancakes);
+        System.out.println("Unmet orders: " + Math.max(0, totalUserPancakes - shopkeeperPancakes));
+    }
+
+    private static String getCurrentTime() {
+        return LocalDateTime.now().toString();    }
+}
+
